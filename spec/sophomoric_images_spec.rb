01deeb2069e6406b501fd2fdfa119115d1ca9f22
Adrian Cann <acann@booster.com>
2015-01-04 19:18:16 -0500

    minor refactor for convention

--- spec/sophomoric_images_spec.rb
+++ spec/sophomoric_images_spec.rb
@@ -9,22 +9,23 @@ describe Croppable do
     Testing.new
   end
 
-  describe "four readable ivars" do
-    [:crop_x, :crop_y, :crop_w, :crop_h].each do |attr|
-      it "#{attr} is nil when nothing is set" do
+  [:crop_x, :crop_y, :crop_w, :crop_h].each do |attr|
+    describe "##{attr}" do
+      it "is nil when nothing is set" do
         expect(instance.send(attr)).to eq(nil)
       end
     end
   end
 
-  describe "four writable ivars" do
-    [:crop_x, :crop_y, :crop_w, :crop_h].each do |attr|
-      it "#{attr} can be set to a number" do
+
+  [:crop_x, :crop_y, :crop_w, :crop_h].each do |attr|
+    describe "##{attr}=" do
+      it "can be set to a number" do
         instance.send((attr.to_s + "="), 10)
         expect(instance.send(attr)).to eq(10)
       end
     end
   end
 
-
+  describe "#cropping"
 end
